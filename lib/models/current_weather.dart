19d36c0ca7f9ed50256bced07c62bed4f57a03f7
JacquelineTanguay <133934614+JacquelineTanguay@users.noreply.github.com>
2025-05-24 23:35:28 -0600

    initial commit

---
+++ lib/models/current_weather.dart
@@ -0,0 +1,18 @@
+// This file defines the CurrentWeather class, which represents the current weather data for a specific city.
+// It includes properties for the city name, weather description, current temperature,
+
+class _CurrentWeather{
+  late String city;
+  late String description;
+  late double currentTemp;
+  final DateTime currentTime = DateTime.now();
+  DateTime? sunrise;
+  DateTime? sunset;
+}
+if (_CurrentWeather)
+
+
+
+catch (e) {
+
+}
